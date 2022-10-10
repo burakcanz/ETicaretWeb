f202c5d8fb951e537720dc91fccc89f9767e01d7
3060 <3060@DESKTOP-8D2M65K>
2022-10-10 20:57:40 +0300

    Add project files.

---
+++ ETicaret.EntityLayer/Concrate/Urun.cs
@@ -0,0 +1,20 @@
+﻿using ETicaret.EntityLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaret.EntityLayer.Concrate
+{
+    public class Urun :IEntity
+    {
+       public int UrunId { get; set; }
+        public int UrunAdi { get; set; }
+        public int UrunKodu { get; set; }
+        public int UrunAcıklama { get; set; }
+        public int AltKategoriId { get; set; }
+
+        
+    }
+}
